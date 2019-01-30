e8f35e35477f6e28b60245fb6b0b36157e17d54d
dev <dev@decent.bet>
2019-01-30 18:40:47 +0400

    add interfaces for quest contract functions, add libquest contract

--- contracts/quest/interfaces/IQuest.sol
+++ contracts/quest/interfaces/IQuest.sol
@@ -1,6 +1,50 @@
-pragma solidity ^0.4.0;
+pragma solidity 0.5.0;
 
 contract IQuest {
+
+    /**
+    * Allows an admin to add a quest
+    * @param id Unique quest ID
+    * @param entryFee Amount to pay in DBETs for quest entry
+    * @param timeToComplete Maximum time for user to complete quest
+    * @param prize Prize in DBETs to payout to winners
+    * @return Whether the quest was added
+    */
+    function addQuest(
+        bytes32 id,
+        uint256 entryFee,
+        uint256 timeToComplete,
+        uint256 prize
+    ) public returns (bool);
+
+    /**
+    * Pays for a quest as a user
+    * @param id Unique quest ID
+    * @return Whether the quest was paid for
+    */
+    function payForQuest(
+        bytes32 id
+    ) public returns (bool);
+
+    /**
+    * Allows the platform to set the quest outcome for a user playing a quest
+    * @param id Unique quest ID
+    * @param user User playing quest
+    * @return Whether quest outcome was set
+    */
+    function setQuestOutcome(
+        bytes32 id,
+        address user
+    ) public returns (bool);
+
+    /**
+    * Allows users who've completed quests to claim their quest payout
+    * @param id Unique quest ID
+    * @return Whether the quest payout was claimed
+    */
+    function claimQuestPayout(
+        bytes32 id
+    ) public returns (bool);
 
     /**
     * Adds an admin to the Quest contract
