1507739656fdd327d94beb4b41c4f8f645455e5b
dev <dev@decent.bet>
2019-02-03 01:05:38 +0400

    add admin tests

--- test/Quest.js
+++ test/Quest.js
@@ -27,4 +27,36 @@ contract('Options', accounts => {
         token = await contracts.DBETVETToken.deployed()
     })
 
+    it('throws if non-owners add an admin', async () => {
+        await utils.assertFail(
+            quest.addAdmin(user2, {from: user1})
+        )
+    })
+
+    it('allows owners to add admins', async () => {
+        await quest.addAdmin(user2, {from: owner})
+        const isUser2Admin = await quest.admins(user2)
+        assert.equal(
+            isUser2Admin,
+            true
+        )
+    })
+
+    it('throws if non-owners remove admins', async () => {
+        await utils.assertFail(
+            quest.removeAdmin(user2, {from: user1})
+        )
+    })
+
+    it('allows owners to remove admins', async () => {
+        await quest.removeAdmin(user2, {from: owner})
+
+        const isUser2Admin = await quest.admins(user2)
+
+        assert.equal(
+            isUser2Admin,
+            false
+        )
+    })
+
 })
