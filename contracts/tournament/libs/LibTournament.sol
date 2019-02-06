13b9cfa0f30c85e81a62d944b138360a13d6504a
dev <dev@decent.bet>
2019-02-07 01:13:37 +0400

    add tournament struct

--- contracts/tournament/libs/LibTournament.sol
+++ contracts/tournament/libs/LibTournament.sol
@@ -2,9 +2,15 @@ pragma solidity 0.5.0;
 
 contract LibTournament {
 
-    constructor ()
-    public {
-
+    struct Tournament {
+        // Entry fee
+        uint256 entryFee;
+        // Unique id of prize table
+        bytes32 prizeTable;
+        // Participants in tournament
+        address[] participants;
+        // Final standings
+        address[] finalStandings;
     }
 
 }
