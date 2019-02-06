c7984320da841535345d28a303c19fc94d57ebad
dev <dev@decent.bet>
2019-02-07 00:34:53 +0400

    add tournament contract stubs

---
+++ contracts/tournament/Tournament.sol
@@ -0,0 +1,10 @@
+pragma solidity 0.5.0;
+
+contract Tournament {
+
+    constructor ()
+    public {
+
+    }
+
+}
---
+++ contracts/tournament/libs/LibTournament.sol
@@ -0,0 +1,10 @@
+pragma solidity 0.5.0;
+
+contract LibTournament {
+
+    constructor ()
+    public {
+
+    }
+
+}
