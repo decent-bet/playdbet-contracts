d23e5a9c20b05d0b787e50304fe11df2f4d101dc
dev <dev@decent.bet>
2019-02-07 01:05:59 +0400

    define tournament contract interface

--- contracts/tournament/interfaces/ITournament.sol
+++ contracts/tournament/interfaces/ITournament.sol
@@ -2,9 +2,29 @@ pragma solidity 0.5.0;
 
 contract ITournament {
 
-    constructor ()
-    public {
+    /**
+    * Creates a prize table that can be used for a tournament
+    */
+    function createPrizeTable() public returns (bool);
 
-    }
+    /**
+    * Creates a tournament that users can enter
+    */
+    function createTournament() public returns (bool);
+
+    /**
+    * Allows users to enter a tournament by paying the listed entry fee
+    */
+    function enterTournament() public returns (bool);
+
+    /**
+    * Allows the admin to complete the tournament by publishing the final standings
+    */
+    function completeTournament() public returns (bool);
+
+    /**
+    * Allows users to claim their tournament prizes
+    */
+    function claimTournamentPrize() public returns (bool);
 
 }
