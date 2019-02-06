078a181d724e7996b342e5a3562a3e81612f1817
dev <dev@decent.bet>
2019-02-07 01:21:14 +0400

    add tournament struct and add parameters to functions in tournament interface

--- contracts/tournament/interfaces/ITournament.sol
+++ contracts/tournament/interfaces/ITournament.sol
@@ -4,27 +4,52 @@ contract ITournament {
 
     /**
     * Creates a prize table that can be used for a tournament
+    * @param table array of integers representing redeemable prize money for each position represented by indices in the array
+    * @return unique ID of the prize table
     */
-    function createPrizeTable() public returns (bool);
+    function createPrizeTable(
+        uint256[] table
+    ) public returns (bytes32);
 
     /**
     * Creates a tournament that users can enter
+    * @param entryFee fee to enter the tournament
+    * @param maxParticipants the maximum number of participants for the tournament
+    * @param prizeTable unique ID of prize table to be used for the tournament
+    * @return unique ID of the created tournament
     */
-    function createTournament() public returns (bool);
+    function createTournament(
+        uint256 entryFee,
+        uint256 maxParticipants,
+        bytes32 prizeTable
+    ) public returns (bytes32);
 
     /**
     * Allows users to enter a tournament by paying the listed entry fee
+    * @param id unique ID of the tournament
     */
-    function enterTournament() public returns (bool);
+    function enterTournament(
+        bytes32 id
+    ) public returns (bool);
 
     /**
     * Allows the admin to complete the tournament by publishing the final standings
+    * @param id unique ID of the tournament
+    * @param finalStandings final standings of participants in the tournament
+    * @return whether the tournament was completed
     */
-    function completeTournament() public returns (bool);
+    function completeTournament(
+        bytes32 id,
+        address[] finalStandings
+    ) public returns (bool);
 
     /**
     * Allows users to claim their tournament prizes
+    * @param id unique ID of the tournament
+    * @return whether the tournament prize was claimed
     */
-    function claimTournamentPrize() public returns (bool);
+    function claimTournamentPrize(
+        bytes32 id
+    ) public returns (bool);
 
 }
--- contracts/tournament/libs/LibTournament.sol
+++ contracts/tournament/libs/LibTournament.sol
@@ -5,6 +5,8 @@ contract LibTournament {
     struct Tournament {
         // Entry fee
         uint256 entryFee;
+        // Maximum participants
+        uint256 maxParticipants;
         // Unique id of prize table
         bytes32 prizeTable;
         // Participants in tournament
