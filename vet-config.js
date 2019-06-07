c58131bb6165c8824f60239fcd3b44fd4091330c
dev <dev@decent.bet>
2019-06-07 23:34:30 +0400

    add mainnet migration script and fix migration to support mainnet

--- vet-config.js
+++ vet-config.js
@@ -1,3 +1,23 @@
+require('dotenv').config()
+
+const {
+    MAINNET_HOST,
+    MAINNET_PORT,
+    MAINNET_GAS_PRICE,
+    MAINNET_FROM,
+    MAINNET_PRIVATE_KEY
+} = process.env
+
+if (
+    MAINNET_PRIVATE_KEY && (
+        typeof MAINNET_PRIVATE_KEY !== 'string' ||
+        MAINNET_PRIVATE_KEY.indexOf(',') === -1 ||
+        MAINNET_PRIVATE_KEY.split(',').length !== 2
+    )
+)
+    throw new Error('Invalid main-net private key. Must be a string key or multiple keys separate by commas')
+
+
 module.exports = {
     chains: {
         testnet: {
@@ -15,6 +35,14 @@ module.exports = {
             gasPrice: '100000000000',
             from: '0x7567d83b7b8d80addcb281a71d54fc7b3364ffed',
             privateKey: '0xdce1443bd2ef0c2631adc1c67e5c93f13dc23a41c18b536effbbdcbcdb96fb65'
+        },
+        mainnet: {
+            host: MAINNET_HOST,
+            port: MAINNET_PORT,
+            chain_tag: "0x4a",
+            gasPrice: MAINNET_GAS_PRICE,
+            from: MAINNET_FROM,
+            privateKey: MAINNET_PRIVATE_KEY
         }
     }
 }
