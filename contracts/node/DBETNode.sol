44ade839708e0218f22e634ac3c3d0518a62903c
dev <dev@decent.bet>
2019-09-16 19:19:19 +0400

    add dbet node structs, addType()

--- contracts/node/DBETNode.sol
+++ contracts/node/DBETNode.sol
@@ -13,9 +13,81 @@ contract DBETNode is
 IDBETNode,
 LibDBETNode {
 
-    constructor()
+    address public owner;
+
+    // Admin contract
+    Admin public admin;
+    // Token contract
+    ERC20 public token;
+
+    // Maps incremented indices of added node types to NodeType structs
+    mapping (uint256 => NodeType) public nodeTypes;
+    // Auto-increment on addition of node types
+    uint256 public nodeTypeCount;
+
+    event LogNewNode(
+
+    );
+    event LogNewNodeType(
+        uint256 id
+    );
+
+    constructor(
+        address _admin,
+        address _token
+    )
+    public {
+        owner = msg.sender;
+        admin = Admin(_admin);
+        token = ERC20(_token);
+    }
+
+    function create(
+    )
     public {
 
+    }
+
+    function destroy()
+    public {
+
+    }
+
+    /**
+    * Adds a new node type
+    */
+    function addType(
+        string name,
+        uint256 tokenThreshold,
+        uint256 timeThreshold
+    )
+    public {
+        // Sender must be an admin
+        require(
+            admin.admins(msg.sender),
+            "INVALID_SENDER"
+        );
+        // Name cannot be empty
+        require(
+            name != "",
+            "INVALID_NAME"
+        );
+        // Must be a non-zero token threshold
+        require(
+            tokenThreshold > 0,
+            "INVALID_TOKEN_THRESHOLD"
+        );
+        // Must be a non-zero time threshold
+        require(
+            timeThreshold > 0,
+            "INVALID_TIME_THRESHOLD"
+        );
+        nodeTypes[nodeTypeCount] = NodeType({
+            name: name,
+            tokenThreshold: tokenThreshold,
+            timeThreshold: timeThreshold
+        });
+        emit LogNewNodeType(nodeTypeCount++);
     }
 
 }
--- contracts/node/libs/LibDBETNode.sol
+++ contracts/node/libs/LibDBETNode.sol
@@ -2,6 +2,26 @@ pragma solidity 0.5.8;
 
 contract LibDBETNode {
 
+    // Defines different types of DBET nodes that can be run
+    struct NodeType {
+        // Name of node
+        string name;
+        // Amount of tokens to lock
+        uint256 tokenThreshold;
+        // Minimum amount of time in seconds to hold tokens to activate node
+        uint256 timeThreshold;
+    }
 
+    // User node
+    struct Node {
+        // Unique node ID
+        uint256 id;
+        // Type of node
+        NodeType type;
+        // DBETs locked as deposit
+        uint256 deposit;
+        // Unix timestamp during creation
+        uint256 creationTime;
+    }
 
 }
